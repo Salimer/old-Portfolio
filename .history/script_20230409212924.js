b69569d568ec8bc3bc5688ba1f785aa21aa1508b
Salimer <52242629+Salimer@users.noreply.github.com>
2023-04-10 00:48:48 +0300

    Add script link to index.html

---
+++ .history/script_20230409212924.js
@@ -0,0 +1,2 @@
+let hamburger = document.querySelector('#nav');
+
---
+++ .history/script_20230409213219.js
@@ -0,0 +1,7 @@
+let hamburger = document.querySelector('#nav');
+
+function display() {
+    alert('It was clicked!');
+}
+
+hamburger.addEventListener('cleck', display);
