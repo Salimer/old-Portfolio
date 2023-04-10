28704b994fdd07e106122624233d21cdc5534976
Salimer <52242629+Salimer@users.noreply.github.com>
2023-04-10 16:45:29 +0300

    Fix linters

--- .history/script_20230410164116.js
+++ .history/script_20230410164116.js
@@ -4,10 +4,12 @@ const navItems = document.querySelectorAll('.mobile-menu li');
 
 hamburger.addEventListener('click', () => {
   div.classList.toggle('show');
-});
+});  
 
 navItems.forEach((element) => {
   element.addEventListener('click', () => {
     div.classList.toggle('show');
   });
 });
+
+
